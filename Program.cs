c1f44cf361ac57438badb7ae4af5695820cd3512
David Smith <cpmpdavidsmith@icloud.com>
2024-01-26 12:28:47 -0500

    wefijw

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace SpringClean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dogs = { "K 9", "Brian Griffin", "Scooby Doo", "Old Yeller", "Rin Tin Tin", "Rambo", "Lassie", "Snoopy" };
+
+            var dogSpaces = from dog in dogs
+                            where dog.Contains(" ")
+                            orderby dog descending
+                            select dog;
+
+            foreach (var i in dogSpaces)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
